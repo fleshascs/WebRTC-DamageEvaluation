ef466e01a8fc3938911b8d8a080e3adc68e46494
Karolis Kaseta <kkas@danskebank.lt>
2021-04-10 21:06:53 +0300

    validate file upload

--- backend/rooms/uploads.controller.js
+++ backend/rooms/uploads.controller.js
@@ -1,5 +1,4 @@
-﻿const roomService = require('./room.service');
-const uploadService = require('./uploads.service');
+﻿const uploadService = require('./uploads.service');
 
 module.exports = {
   download,
@@ -23,10 +22,16 @@ function upload(req, res, next) {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
+
+  if (!req.files.sampleFile.name.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = 'Only image files are allowed!';
+    return next(new Error('Only image files are allowed!'), false);
+  }
+
   const roomId = req.params.roomId;
-  roomService
+  uploadService
     .upload(req.files, req.params.roomId)
-    .then(({ publicPath }) => roomService.saveUploadToDB(publicPath, req.user.id, roomId))
+    .then(({ publicPath }) => uploadService.saveUploadToDB(publicPath, req.user.id, roomId))
     .then((file) => res.json(file))
     .catch(next);
 }
